8f4ed1094484417ac60dfaac24aab8b447b383b4
Joseph Diza <josephm.diza@gmail.com>
2021-01-08 23:29:54 -0800

    Parallelize getAllHTML files

--- minis/downloadAllMinis.py
+++ minis/downloadAllMinis.py
@@ -3,6 +3,7 @@ import re
 import os
 import requests
 import cProfile
+import threading, queue
 
 # downloadAllMinis.py - Downloads .stl files of miniatures
 
@@ -14,12 +15,11 @@ import cProfile
 # 6. Create master index hash table.
 # 7. Look up each url, get the file, download and save as filename
 
-# f = open("creations", "r");
-# soup = BeautifulSoup(f, 'html.parser')
 
-# path = "./html"
-
-site = "https://www.shapeways.com/designer/mz4250/creations"
+def createSoup(filename):
+    f = open(filename, "r") 
+    soup = BeautifulSoup(f, 'html.parser')
+    return soup
 
 def getPages(soup):
     regexp = "(\/designer\/mz4250\/creations\?s=\d{0,4}#more-products)"
@@ -46,22 +46,55 @@ def getHTML(site):
 # def runOnNewThread():
     # thread_count += 1
 
-def getAllHTML(end, site):
-    index = 1
-    pageIndex = 48
-    while (pageIndex < end):
-        currentPage = (f'{site}?s={pageIndex}')
-        pageIndex += 48
-        saveAs = (f'{site}/mz4250-creations-page-{index}')
+# pages = queue.Queue()
+# saved = queue.Queue()
 
-        print(f'currentPage: {currentPage}')
-        print(f'pageIndex: {pageIndex}')
-        print(f'saveAs: {saveAs}')
+def getHTMLParallel(currentPage, saveAs):
+    # if (pages is not None and saved is not None):
+        # writeToFile(getHTML(pages.get()), saved.get())
+    if (currentPage is not None and saveAs is not None):
+        print(f"Got HTML page: {currentPage}")
+        print(f"Wrote to file: {saveAs}")
+        # html = getHTML(currentPage)
+        # writeToFile(html, saveAs)
+
+def getAllHTML(end, site, directory):
+    index = 1
+    pageIndex = 0
+    while (pageIndex < end):
+        currentPage = (f'{site}?s={pageIndex}') 
+        pageIndex += 48
+        saveAs = (f'{directory}/mz4250-creations-page-{index}')
+
+        print(f'currentPage : {currentPage}')
+        print(f'pageIndex   : {pageIndex}')
+        print(f'saveAs      : {saveAs}')
 
         if (not os.path.exists(saveAs)):
-            html = getHTML(currentPage)
-            writeToFile(html, saveAs)
+            # thread_count = threading.active_count()
+            # if (thread_count < 5):
+            if (threading.active_count() <= 5):
+                # print(f"Creating new worker: worker_count: {thread_count}")
+                worker = threading.Thread(target=getHTMLParallel, args=(currentPage, saveAs))
+                worker.start()
+            # elif (thread_count == 4 or thread_count > 4):
+            else:
+                for thread in threading.enumerate():
+                    if (thread is threading.main_thread()): 
+                        continue
+                    else: 
+                        thread.join()
+            # else:
+            # html = getHTML(currentPage)
+            # writeToFile(html, saveAs)
         index += 1
+    currentPage = (f'{site}?s={pageIndex}') 
+    pageIndex = end 
+    saveAs = (f'{directory}/mz4250-creations-page-{index}')
+
+    getHTMLParallel(currentPage, saveAs)
+    # html = getHTML(currentPage)
+    # writeToFile(html, saveAs)
 
 
 def getEnd(tag):
@@ -70,8 +103,16 @@ def getEnd(tag):
     index = regexp.search(tag[5]['href'])
     return int(index.group(0))
 
+
+# path = "./html"
+site = "https://www.shapeways.com/designer/mz4250/creations"
+
+# x = threading.Thread(target=getHTMLParallel, args=(1,))
 # writeToFile(getHTMLPage(site), path + "/mz4250-creations-page-1")
-# end = getEnd(getPages(soup))
 # getAllHTML(end)
+
+
+end = getEnd(getPages(createSoup("creations.html")))
+getAllHTML(end, site, "./html")
 
 # cProfile.run('getAllHTML(end)')
