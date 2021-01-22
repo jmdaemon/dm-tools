ad4053d21174ec61a9f0b069314b6f455d30dd1f
Joseph Diza <josephm.diza@gmail.com>
2021-01-21 19:32:51 -0800

    Extract tags from miniatures

--- minis/script.py
+++ minis/script.py
@@ -1,6 +1,7 @@
 #/usr/bin/python3.9
 
 from downloader import *
+import os
 
 def setupMetadata(soup):
     linksList   = extractMiniatureLinks(soup)
@@ -20,25 +21,26 @@ def downloadAllMinis():
 def createMasterIndex():
     downloader.createIndex()
 
-def downloadAllMiniMetadata():
-    print(f"============ Tag Extraction ============")
+def getEnd(offset = 0): 
     end = extractEnd(extractProductPages(createSoup("creations.html")))
-    print(f"Last Page Offset: {end}")
-    offset = 0
     pagesEnd = (len([offset for offset in ([*range(offset, end, 48)] + [end])]))
+    print(f"Last Page Offset: {end}")
     print(f"Last Page Index: {pagesEnd}")
+    return end, pagesEnd
 
-    base = "./html"
+def indicesToRange(pagesEnd): 
+    return ([*range(1, pagesEnd)] + [pagesEnd])
+
+def downloadAllMiniMetadata(base = "./html"):
+    print(f"============ Tag Extraction ============")
+    end, pagesEnd = getEnd()
     directory = f"{base}/products"
-    pageIndexes = [f"{directory}/pages-{index}" for index in ([*range(1, pagesEnd)] + [pagesEnd])]
-    # pageIndexes.add(pagesEnd)
-    print (pageIndexes)
+    pageIndexes = [f"{directory}/pages-{index}" for index in indicesToRange]
+    print(pageIndexes)
     print(f"")
-
+    
     currentIndex = 1
     pageIndex = 0
-    stopAtPageEnd = pagesEnd - 1
-    print(stopAtPageEnd)
     while (currentIndex <= pagesEnd and pageIndex < pagesEnd):
         soup = createSoup(f"{base}/mz4250-creations-page-{currentIndex}")
         metadata = setupMetadata(soup)
@@ -49,7 +51,34 @@ def downloadAllMiniMetadata():
         pageIndex += 1
     print("")
 
+def downloadAllMiniatureTags(directory = "./html/products"):
+    index = 1
+    end, pagesEnd = getEnd()
+    pages = [f"{directory}/pages-{index}" for index in indicesToRange(pagesEnd)]
+    currentIndex = 1
+    pageIndex = 0
+    while (currentIndex <= pagesEnd and pageIndex < pagesEnd):
+        print("Page Index       : {currentIndex}")
+        currentPageDir = pages[pageIndex]
+        print(f"Page Directory  : {currentPageDir}")
+        print(os.listdir(f"{currentPageDir}")) 
+        print(f"")
+        pages = os.listdir(f"{currentPageDir}")
+
+        for page in pages:
+            print("============ Tags ============")
+            print(f"Miniature Page: {page}")
+            soup = createSoup(f"{currentPageDir}/{page}")
+            tags = extractMiniatureTags(soup)
+            for tag in tags:
+                print(f"Tag: {tag}") 
+            print(f"")
+
+        currentIndex += 1
+        pageIndex += 1
+
 # downloadHTMLIndices()
 # downloadAllMinis()
 # createMasterIndex()
 # downloadAllMiniMetadata()
+downloadAllMiniatureTags()
